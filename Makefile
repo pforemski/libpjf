c653572d80456c2408d4089b916a165744cb57e5
Pawel Foremski <pjf@asn.pl>
2008-11-21 13:30:20 +0100

    generic, a bit better "make install"; dont build "example" by default (rarely needed)

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 CFLAGS =
 LDFLAGS =
 
+ME=libasn
 C_OBJECTS=lib.o sfork.o regex.o thash.o tlist.o xstr.o wstr.o mmatic.o tsort.o fifos.o fc.o
-TARGETS=libasn.so libasn.a example
+TARGETS=libasn.so libasn.a
 
 include rules.mk
 
@@ -18,8 +19,4 @@ libasn.a: $(C_OBJECTS) pcre/.libs/libpcre.a
 example: example.o libasn.a pcre/.libs/libpcre.a
 	$(CC) $(CFLAGS) example.c -o example libasn.a pcre/.libs/libpcre.a
 
-install: all
-	install -m 755 -d $(PKGDST)/include/libasn
-	install -m 644 *.h $(PKGDST)/include/libasn
-	install -m 755 -d $(PKGDST)/lib
-	install -m 755 libasn.so $(PKGDST)/lib
+install: install-std
--- rules.mk
+++ rules.mk
@@ -1,9 +1,10 @@
 #
-# ASN rules.mk, v. 2
+# ASN rules.mk, v. 3
 #
 
 CC ?= gcc
 AR ?= ar
+ME ?= $(shell basename `pwd`)
 
 CFLAGS += -std=gnu99 -g -Wall -pedantic -fPIC -Dinline='inline __attribute__ ((gnu_inline))' $(CFLAGS_ADD)
 LDFLAGS += -Wall -pedantic $(LDFLAGS_ADD)
@@ -38,5 +39,16 @@ doc:
 	-rm -fr doc
 	mkdir -p doc
 	doxygen doxygen.conf
+
+install-std: all
+	install -m 755 -d $(PKGDST)/include/$(ME)
+	install -m 644 *.h $(PKGDST)/include/$(ME)
+	install -m 755 -d $(PKGDST)/lib
+	for i in $(TARGETS); do \
+		[ "$${i##*.}" = "so" ] && install -m 755 $$i $(PKGDST)/lib; \
+		[ "$${i##*.}" = "a" ]  && install -m 644 $$i $(PKGDST)/lib; \
+	done || true
+	install -m 755 -d $(PKGDST)/bin
+	for i in $(TARGETS); do test "$${i##*.}" = "$$i" && install -m 755 $$i $(PKGDST)/bin; done || true
 
 .PHONY: doc
