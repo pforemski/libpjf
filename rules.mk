05ac6cc876104174f8285a2713f1d151040d9a02
Łukasz 'Sil2100' Zemczak <sil2100@asn.pl>
2008-12-11 15:41:47 +0100

    New rules.mk applied here too.

--- rules.mk
+++ rules.mk
@@ -1,5 +1,5 @@
 #
-# ASN rules.mk, v. 3
+# ASN rules.mk, v. 4
 #
 
 CC ?= gcc
@@ -24,11 +24,20 @@ endif
 PREFIX ?= /usr
 PKGDST = $(DESTDIR)$(PREFIX)
 
+# for make version.h
+GITVER = $(shell git describe 2>/dev/null)
+TARVER = $(shell cat VERSION 2>/dev/null)
+ifneq (,$(GITVER))
+VERSION ?= $(GITVER)
+else
+VERSION ?= $(TARVER)
+endif
+
 default: all
-all: $(TARGETS)
+all: version.h $(TARGETS)
 
 clean:
-	-rm -f *.o $(TARGETS) *.core core
+	-rm -f *.o $(TARGETS) *.core core version.h
 
 .SUFFIXES: .c
 
@@ -39,6 +48,9 @@ doc:
 	-rm -fr doc
 	mkdir -p doc
 	doxygen doxygen.conf
+
+version.h:
+	@echo '#define VERSION "$(VERSION)"' >$@
 
 install-std: all
 	install -m 755 -d $(PKGDST)/include/$(ME)
@@ -51,4 +63,16 @@ install-std: all
 	install -m 755 -d $(PKGDST)/bin
 	for i in $(TARGETS); do test "$${i##*.}" = "$$i" && install -m 755 $$i $(PKGDST)/bin; done || true
 
-.PHONY: doc
+install-lns: all
+	mkdir -m 755 -p $(PKGDST)/{include,lib}/$(ME)
+	ln -s $(PWD)/*.h $(PKGDST)/include/$(ME)/
+	for i in $(TARGETS); do \
+		[ "$${i##*.}" = "so" ] && ln -s $(PWD)/$$i $(PKGDST)/lib/; \
+		[ "$${i##*.}" = "a" ]  && ln -s $(PWD)/$$i $(PKGDST)/lib/; \
+	done || true
+	mkdir -m 755 -p $(PKGDST)/bin
+	for i in $(TARGETS); do \
+		[ "$${i##*.}" = "$$i" ] && ln -s $(PWD)/$$i $(PKGDST)/bin/;\
+	done || true
+
+.PHONY: version.h doc
